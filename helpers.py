84c7ba697a0f6e4935626652b24065dd3e7330f2
Albert Francisco <albertfrancisco124@gmail.com>
2022-08-15 15:57:22 +0800

    Fix user details - Add password property - Make email lowercase

--- helpers.py
+++ helpers.py
@@ -8,9 +8,10 @@ NTC_REGIONS = len(ntc_region_choices)
 
 def _get_user(data):
     ntc_region = data[0]
-    user_obj = data[1]
+    user_obj: RandomUser = data[1]
     user = {'firstname': user_obj.get_first_name(), 'lastname': user_obj.get_last_name(),
-            'email': f"{user_obj.get_full_name().replace(' ', '.')}@example.com",
+            'email': f"{user_obj.get_full_name(False).replace(' ', '.')}@example.com",
+            'password': "netmesh1234",
             'ntc_region': ntc_region[0]}
     return user
 
@@ -31,6 +32,9 @@ def generate_users(n):
 
     return list(users)
 
+def register_users(users):
+    # TODO: submit generated random users to results server
+    pass
 
 if __name__ == "__main__":
     rand_users = generate_users(20)
