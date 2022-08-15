a19c1814d04443040c48c922c58e418023b676a7
Albert Francisco <albertfrancisco124@gmail.com>
2022-08-15 17:42:34 +0800

    Insert users in rs

--- helpers.py
+++ helpers.py
@@ -1,14 +1,17 @@
+import json
+import requests
 from randomuser import RandomUser
 
 from ntc_regions import ntc_region_choices
 
 NTC_REGIONS = len(ntc_region_choices)
 
+RESULTS_SERVER_API_URL = "http://202.90.159.48/api"
 
 def _get_user(data):
     ntc_region = data[0]
     user_obj: RandomUser = data[1]
-    user = {'firstname': user_obj.get_first_name(), 'lastname': user_obj.get_last_name(),
+    user = {'first_name': user_obj.get_first_name(), 'last_name': user_obj.get_last_name(),
             'email': f"{user_obj.get_full_name(False).replace(' ', '.')}@example.com",
             'password': "netmesh1234",
             'ntc_region': ntc_region[0]}
@@ -26,19 +29,50 @@ def generate_users(n):
         regions.extend(ntc_region_choices * n)
         regions.extend(ntc_region_choices[:not_multiple])
 
-    users = RandomUser.generate_users(n, {'nat': 'ch'})
+    users = RandomUser.generate_users(n, {'nat': 'us'})
     users = map(_get_user, zip(regions, users))
 
     return list(users)
 
+def register_users(users):
+    token = get_admin_token()
 
-def register_user(user):
-    # TODO: submit generated random users to results server
+    if not token:
+        print("no token")
+        return
 
-    pass
+    try:
+        for user in users:
+            r = requests.post(
+                url=f'{RESULTS_SERVER_API_URL}/user/create/',
+                json=user,
+                headers={"Authorization": f"Token {token}"}
+            )
+            r.raise_for_status()
+            
+            print("added to the rs: ", user)
+    except requests.exceptions.RequestException as rx:
+        print(r.content)
 
+def get_admin_token():
+    token = None
+    try:
+        r = requests.post(
+            url=f'{RESULTS_SERVER_API_URL}/user/token/',
+            json={
+                "email": "super@example.com",
+                "password": "netmesh!@#",
+                "client": "admin"
+            }
+        )
+        r.raise_for_status()
+        token = r.json()["token"]
+    except requests.exceptions.RequestException as rx:
+        print("error", rx)
+
+    return token
 
 if __name__ == "__main__":
-    rand_users = generate_users(20)
+    rand_users = generate_users(95)
     for i in rand_users:
         print(i)
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import json
-from helpers import generate_users
+from helpers import generate_users, register_users
 
 # TODO: Post generated users to server
 
 
 if __name__ == "__main__":
     rand_users = generate_users(20)
+    register_users(rand_users)
     # convert to json
-    users_json = json.dumps(rand_users, indent=4)
-    print(users_json)
+    # users_json = json.dumps(rand_users, indent=4)
+    # print(users_json)
